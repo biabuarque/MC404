82ad28cd9ec7c04f4b694bc9e8af049efdc4d329
Ana Beatriz Buarque <130666979+biabuarque@users.noreply.github.com>
2024-11-11 11:42:54 -0300

    amigos. está tocando

--- lab-14/lib.s
+++ lab-14/lib.s
@@ -1,52 +1,39 @@
-.bss
-.align 2
-isr_stack:       
-.skip 1024      
-isr_stack_end: 
-sp_stack:   
-.skip 1024    
-sp_stack_end: 
-
 .text
 .align 2
 
-.set gpt_start, 0xffff0100
-.set gpt_time, 0xffff0104
-.set gpt_generate, 0xffff0108
-.set midi_ch, 0xffff0300
-.set midi_id, 0xffff0302
-.set midi_note, 0xffff0304
-.set midi_vel, 0xffff0305
-.set midi_dur, 0xffff0306
-
 .globl _system_time
-_system_time: .word 0
-
+.globl play_note
 .globl _start
+
 _start:
+# put gpt_handling to mtvec
+la t0, gpt_handling
+csrw mtvec, t0
+
 # initialize program stack and irs stack
 la t0, isr_stack_end
 csrw mscratch, t0
 la sp, sp_stack_end
 
 # initialize gpt
-li a0, gpt_start
-li a1, 1
-sw a1, 0(a0)
-li a0, gpt_generate
-li a1, 100
-sw a1, 0(a0)
+la t0, gpt_start
+sb x0, 0(t0)
+li t1, 100
+sw t1, 8(t0)
 
-# put gpt_handling to mtvec
-la t0, gpt_handling
-csrw mtvec, t0
 # set interrupts to be enabled
 csrr t1, mie 
 li t2, 0x800
+or t1, t1, t2
 csrw mie, t1
+
 csrr t1, mstatus
 ori t1, t1, 0x8
 csrw mstatus, t1
+
+# gpt activation
+li t1, 1
+sb t1, 0(t0)
 
 jal main
 
@@ -69,18 +56,21 @@ sw t3, 44(sp)
 sw t4, 48(sp)
 sw t5, 52(sp)
 sw t6, 56(sp)
-sw ra, 60(sp)
 
-# treat exception
-la a0, _system_time
-lw a1, 0(a0)
+# gpt activation
+la a0, gpt_start
+li a1, 1
+sb a1, 0(a0)
+
+# add 100 to system time
+la a2, _system_time
+lw a1, 0(a2)
 addi a1, a1, 100
-sw a1, 0(a0)
+sw a1, 0(a2)
 
 # set next interrupt
-li a0, gpt_generate
 li a1, 100
-sw a1, 0(a0)
+sw a1, 8(a0)
 
 # restore context
 lw a0, 0(sp)
@@ -98,31 +88,37 @@ lw t3, 44(sp)
 lw t4, 48(sp)
 lw t5, 52(sp)
 lw t6, 56(sp)
-lw ra, 60(sp)
+addi sp, sp, 64
 csrrw sp, mscratch, sp
 mret
 
-.globl play_note # void play_note(int ch, int inst, int note, int vel, int dur); - a0 = ch, a1 = inst, a2 = note, a3 = vel, a4 = dur
+# void play_note(int ch, int inst, int note, int vel, int dur); - a0 = ch, a1 = inst, a2 = note, a3 = vel, a4 = dur
 play_note:
-addi sp, sp, -4
-sw t0, 0(sp)
 
-mv t0, a0
-li a0, midi_ch
-sw t0, 0(a0)
+la t0, midi_start
+sh a1, 2(t0)
+sb a2, 4(t0)
+sb a3, 5(t0)
+sh a4, 6(t0)
+sb a0, 0(t0)
 
-li a0, midi_id
-sw a1, 0(a0)
-
-li a0, midi_note
-sw a2, 0(a0)
-
-li a0, midi_vel
-sw a3, 0(a0)
-
-li a0, midi_dur
-sw a4, 0(a0)
-
-lw t0, 0(sp)
-addi sp, sp, 4
 ret
+
+.data
+.align 2
+.set gpt_start, 0xFFFF0100
+.set midi_start, 0xFFFF0300
+
+.globl _system_time
+.set _system_time, 0
+
+.bss
+.align 4
+
+.globl isr_stack_end
+isr_stack: .skip 1024      
+isr_stack_end: 
+
+.globl sp_stack_end
+sp_stack: .skip 1024    
+sp_stack_end: 
